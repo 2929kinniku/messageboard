73ed8edfce16c24a723edf23e8bbf22541a983d5
kinnikuman <kinnikubukeyashiki@gmail.com>
2018-07-04 06:45:27 +0000

    revised favorites

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,21 @@ class Micropost < ApplicationRecord
   
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :favorites
+  
+  def like(micropost)
+    # self.favorites.find_or_create_by(user_id: current_user)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+
+  def unlike(micropost)
+    # favorite = self.favorites.find_by(user_id: current_user)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+
+  def liking?(micropost)
+    self.likings
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,22 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+  has_many :favorites
+  has_many :favorite_microposts, through: :favorites, source: :micropost
+
+  def like(micropost)
+    # self.favorites.find_or_create_by(user_id: current_user)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+
+  def unlike(micropost)
+    # favorite = self.favorites.find_by(user_id: current_user)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+
+  def liking?(micropost)
+    self.favorite_microposts.include?(micropost)
+  end
 end
